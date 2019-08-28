0816d5712cc803d95f6cebbfac6204b47d8f1511
Barry Scott Rollan <barry.rollan@gmail.com>
2019-08-28 11:38:49 -0400

    Updates

---
+++ precache-manifest.74a14841f52b5600a7d3dd8f74f93965.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32d6d2ddff077258f74254b55e38eeb",
+    "url": "/memory-match-game/index.html"
+  },
+  {
+    "revision": "b42b1c0f473447e6c8cd",
+    "url": "/memory-match-game/static/css/main.9a538d61.chunk.css"
+  },
+  {
+    "revision": "57cdc4663ac7afe78ce2",
+    "url": "/memory-match-game/static/js/2.4441f3fe.chunk.js"
+  },
+  {
+    "revision": "b42b1c0f473447e6c8cd",
+    "url": "/memory-match-game/static/js/main.24e82aa5.chunk.js"
+  },
+  {
+    "revision": "68c6c6d08dfd21df20cd",
+    "url": "/memory-match-game/static/js/runtime~main.1fda4493.js"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/memory-match-game/static/media/Ace1.e6a6d2f8.png"
+  },
+  {
+    "revision": "e6a6d2f8a350079dcad97e814d3559e7",
+    "url": "/memory-match-game/static/media/Ace2.e6a6d2f8.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/memory-match-game/static/media/Eight1.1e9e59b0.png"
+  },
+  {
+    "revision": "1e9e59b0d6f684908f3ad7d2fe942a42",
+    "url": "/memory-match-game/static/media/Eight2.1e9e59b0.png"
+  },
+  {
+    "revision": "24fd440cbb52affc5242a507c9dec4d1",
+    "url": "/memory-match-game/static/media/Five1.24fd440c.png"
+  },
+  {
+    "revision": "24fd440cbb52affc5242a507c9dec4d1",
+    "url": "/memory-match-game/static/media/Five2.24fd440c.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/memory-match-game/static/media/Jack1.1595a3ea.png"
+  },
+  {
+    "revision": "1595a3ea21f452c290cf26af7ae53ede",
+    "url": "/memory-match-game/static/media/Jack2.1595a3ea.png"
+  },
+  {
+    "revision": "6b5b30c00309e3cfae486f543bc1809a",
+    "url": "/memory-match-game/static/media/King1.6b5b30c0.png"
+  },
+  {
+    "revision": "6b5b30c00309e3cfae486f543bc1809a",
+    "url": "/memory-match-game/static/media/King2.6b5b30c0.png"
+  },
+  {
+    "revision": "1d495bb1ebf632dca66a538b61bae19a",
+    "url": "/memory-match-game/static/media/Nine1.1d495bb1.png"
+  },
+  {
+    "revision": "1d495bb1ebf632dca66a538b61bae19a",
+    "url": "/memory-match-game/static/media/Nine2.1d495bb1.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/memory-match-game/static/media/Queen1.63afa62b.png"
+  },
+  {
+    "revision": "63afa62b47efa84b8b87d3e39ef42bc1",
+    "url": "/memory-match-game/static/media/Queen2.63afa62b.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/memory-match-game/static/media/Seven1.4808cc41.png"
+  },
+  {
+    "revision": "4808cc416c976dbd5c3cb629a0f102a5",
+    "url": "/memory-match-game/static/media/Seven2.4808cc41.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/memory-match-game/static/media/Six1.c99b0f04.png"
+  },
+  {
+    "revision": "c99b0f04000841e5f3db53d64b3f7034",
+    "url": "/memory-match-game/static/media/Six2.c99b0f04.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/memory-match-game/static/media/Ten1.b208cd7f.png"
+  },
+  {
+    "revision": "b208cd7f21ce87cf950936ab6fb3e717",
+    "url": "/memory-match-game/static/media/Ten2.b208cd7f.png"
+  },
+  {
+    "revision": "db53954a64b0a647a569a1d3964135ce",
+    "url": "/memory-match-game/static/media/gray_back.db53954a.png"
+  },
+  {
+    "revision": "338eceb93dc6748ada207615132edeee",
+    "url": "/memory-match-game/static/media/x.338eceb9.png"
+  }
+]);
