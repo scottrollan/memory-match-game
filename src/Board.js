a68b822723b8ff01914b69ad2e937d3b6fe521cc
Barry Scott Rollan <barry.rollan@gmail.com>
2019-09-04 10:33:29 -0400

    moved deck up to Game.js along with shuffle function() which I placed inside of the the level selector function, so that I could do away with componentWillMount in Board.js

--- src/Board.js
+++ src/Board.js
@@ -2,26 +2,6 @@ import React from 'react';
 import './board.css';
 import Discard from './Discard';
 import Card from './Card';
-import Ace1 from './media/Ace1.png';
-import King1 from './media/King1.png';
-import Queen1 from './media/Queen1.png';
-import Jack1 from './media/Jack1.png';
-import Ten1 from './media/Ten1.png';
-import Nine1 from './media/Nine1.png';
-import Eight1 from './media/Eight1.png';
-import Seven1 from './media/Seven1.png';
-import Six1 from './media/Six1.png';
-import Five1 from './media/Five1.png';
-import Ace2 from './media/Ace2.png';
-import King2 from './media/King2.png';
-import Queen2 from './media/Queen2.png';
-import Jack2 from './media/Jack2.png';
-import Ten2 from './media/Ten2.png';
-import Nine2 from './media/Nine2.png';
-import Eight2 from './media/Eight2.png';
-import Seven2 from './media/Seven2.png';
-import Six2 from './media/Six2.png';
-import Five2 from './media/Five2.png';
 
 const boardStyle = {
     width: '100vw',
@@ -33,12 +13,6 @@ class Board extends React.Component {
     constructor(props) {
         super(props);   
         this.state = {
-            deck: [
-                Ace1, Ace2, King1, King2, Queen1,
-                Queen2, Jack1, Jack2, Ten1, Ten2,
-                Nine1, Nine2, Eight1, Eight2, Seven1,
-                Seven2, Six1, Six2, Five1, Five2
-            ],
             flippedCards: 0,
             flippedOne: '',
             flippedStoreOne: '',
@@ -55,7 +29,6 @@ class Board extends React.Component {
             Jack: '70.5',
             Queen: '80.3',
             King: '90.1',
-            xPosition: '',
             faceValue: ''
         };
     };
@@ -80,8 +53,6 @@ class Board extends React.Component {
             unmatched2X.style.visibility = "visible";
         }, 500);        
         setTimeout(() => {
-            // unmatched1.classList.remove("shake");
-            // unmatched2.classList.remove("shake");
             unmatched1X.style.visibility = "hidden";
             unmatched2X.style.visibility = "hidden";
         }, 500);
@@ -125,7 +96,7 @@ class Board extends React.Component {
         const cardStoreValue = event.target.dataset.value; //e.g. "Ace1" or "Ace2"
         const cardValue = event.target.dataset.value.slice(0,-1); //e.g. "Ace"
         const cardValueDiscarded = `${cardValue}Discarded`
-        this.setState({ faceValue: cardValueDiscarded });
+        this.setState({ faceValue: cardValueDiscarded }); // "AceDiscarded"
         this.state.flippedCards === 0 ?  //if no cards have been flipped yet...
             this.setState({ flippedOne: cardValue, flippedStoreOne: cardStoreValue }) : //assign clicked card to flippedOne, otherwise...
             this.setState({ flippedTwo: cardValue, flippedStoreTwo: cardStoreValue, });  //assign clicked card to flippedTwo
@@ -137,43 +108,27 @@ class Board extends React.Component {
         }, 500);
     };
 
-    shuffleArray = (array) => {
-        let i = 0;
-        for (i = array.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        };
-    };
-
-    componentWillMount() {
-    this.shuffleArray(this.state.deck);
-    };
-
     render(props) {
         return(
                 <div 
                     id="gameBoard"
                     style={boardStyle}>
                     <Discard />
-                    {this.state.deck.map(data => (
+                    {this.props.deck.map(data => (
                         <Card   
                             key={data} 
                             src={data} 
-                            // id={index}
                             id={data.substr(14).slice(0, -13)} //e.g. Ace1, Ace2    
                             value={data.substr(14).slice(0, -13)}  //e.g. Ace1, Ace2
                             time={this.props.time}
                             handleFlip={this.handleFlip}
                             flippedCards={this.state.flippedCards}
-                            // flipped={this.state.flipped}
                         />  
                     ))}
                     
                 </div>
         )
-    }
-
-
-}
+    };
+};
 
 export default Board;
--- src/Game.js
+++ src/Game.js
@@ -8,6 +8,26 @@ import YouWin from './YouWin';
 import YouLose from './YouLose';
 import Pyro from './Pyro';
 import PlayAgain from './PlayAgain';
+import Ace1 from './media/Ace1.png';
+import King1 from './media/King1.png';
+import Queen1 from './media/Queen1.png';
+import Jack1 from './media/Jack1.png';
+import Ten1 from './media/Ten1.png';
+import Nine1 from './media/Nine1.png';
+import Eight1 from './media/Eight1.png';
+import Seven1 from './media/Seven1.png';
+import Six1 from './media/Six1.png';
+import Five1 from './media/Five1.png';
+import Ace2 from './media/Ace2.png';
+import King2 from './media/King2.png';
+import Queen2 from './media/Queen2.png';
+import Jack2 from './media/Jack2.png';
+import Ten2 from './media/Ten2.png';
+import Nine2 from './media/Nine2.png';
+import Eight2 from './media/Eight2.png';
+import Seven2 from './media/Seven2.png';
+import Six2 from './media/Six2.png';
+import Five2 from './media/Five2.png';
 
 class Game extends Component {
   state = {
@@ -15,7 +35,13 @@ class Game extends Component {
     gameStarted: false,
     matchesFound: 0,
     beginningTime: 0,
-    score: 0
+    score: 0,
+    deck: [
+      Ace1, Ace2, King1, King2, Queen1,
+      Queen2, Jack1, Jack2, Ten1, Ten2,
+      Nine1, Nine2, Eight1, Eight2, Seven1,
+      Seven2, Six1, Six2, Five1, Five2
+  ],
   }
   
   addMatch = () => {
@@ -78,6 +104,12 @@ class Game extends Component {
   };
 
   startGame = () => {
+    let i = 0;  // shuffle deck
+    const array = this.state.deck;
+      for (i = array.length - 1; i > 0; i--) {
+          let j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      };
     document.getElementById('pickLevel').style.display = 'none';
     document.getElementById('gameBoard').style.display = 'inline-block';
     if(this.state.gameStarted === false){
@@ -109,7 +141,8 @@ class Game extends Component {
           onClick={this.startGame}
           addMatch={this.addMatch}
           resetMatch={this.resetMatch}
-          time={this.state.time}/>
+          time={this.state.time}
+          deck={this.state.deck}/>
         <Pyro/>
         <PlayAgain/>
       </div>
